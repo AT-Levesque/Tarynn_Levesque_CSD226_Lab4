8cef8c7d9c0dce6d6191ea752f5e53f5f3bb7f3a
fcarella <fred.carella@gmail.com>
2024-03-11 15:43:58 -0400

    Initial commit

---
+++ src/main/java/csd226/lecture8/Lecture8Application.java
@@ -0,0 +1,13 @@
+package csd226.lecture8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture8Application.class, args);
+	}
+
+}
